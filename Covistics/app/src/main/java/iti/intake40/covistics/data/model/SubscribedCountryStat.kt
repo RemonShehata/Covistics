7514121baa89bea0f0bfa3486e7169027b26f9fa
Zeyad El-assal <Zeyad_el3ssal_93@hotmail.com>
2020-05-03 05:14:53 +0200

    first time logic added fix bug in newcases calculation

--- Covistics/app/src/main/java/iti/intake40/covistics/data/model/SubscribedCountryStat.kt
+++ Covistics/app/src/main/java/iti/intake40/covistics/data/model/SubscribedCountryStat.kt
@@ -9,7 +9,7 @@ data class SubscribedCountryStat(
     var country: String,
     @SerializedName("latest_stat_by_country")
     @Expose
-    var countryLastestStat: List<List<SubscribedCountryData>>
+    var countryLastestStat: List<SubscribedCountryData>
 )
 
 data class SubscribedCountryData(
