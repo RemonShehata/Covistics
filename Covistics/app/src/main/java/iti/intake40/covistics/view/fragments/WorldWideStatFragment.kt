68f2725c572a0aa0074b7f8ffd920c867ae35a70
RemonShehata <RemonGerges94@gmail.com>
2020-05-04 10:52:23 +0200

    Finish worldwide fragment design

--- Covistics/app/src/main/java/iti/intake40/covistics/view/fragments/WorldWideStatFragment.kt
+++ Covistics/app/src/main/java/iti/intake40/covistics/view/fragments/WorldWideStatFragment.kt
@@ -13,6 +13,7 @@ import iti.intake40.covistics.R
 import iti.intake40.covistics.data.model.WorldWideStat
 import iti.intake40.covistics.data.network.ApiClient
 import iti.intake40.covistics.viewmodel.MainViewModel
+import kotlinx.android.synthetic.main.fragment_world_wide_stat.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -28,9 +29,16 @@ class WorldWideStatFragment : Fragment() {
     ): View? {
         val viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
         viewModel.liveWorldWideStat.observe(viewLifecycleOwner, Observer {
-            Log.d("WorldwideFragment",it.cases)
+            //Log.d("WorldwideFragment",it.cases)
+            ww_total_cases_tv.text = it.cases
+            ww_new_cases_tv.text = it.newCases
+            ww_active_cases_tv.text = it.activeCases
+            ww_deaths_tv.text = it.deaths
+            ww_new_deaths_tv.text = it.newDeaths
+            ww_total_recoverd_tv.text = it.totalRecovered
         })
         viewModel.getWorldwideData(this)
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_world_wide_stat, container, false)
     }
